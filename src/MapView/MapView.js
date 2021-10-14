9c9f32f684bd3c94c1042a976123bb607a4fe31f
TimWatervoort <twwatervoort@gmail.com>
2021-10-14 15:26:40 -0600

    has pictures and stuff, cool

--- src/MapView/MapView.js
+++ src/MapView/MapView.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import GoogleMapReact from 'google-map-react';
+import { team } from '../dummyData';
+import { getAddressData, getAddressesForMapView } from '../services/googleMaps';
 import './MapView.css';
 
 const TestText = ({ text }) => <div>{text}</div>;
 
+const TeamMemberAvatar = ({ image, name }) => {
+	return <img className='team-member-avatar' src={image} alt={name} />;
+}
+
 const MapView = (props) => {
+	const [teamMembers, setTeamMembers] = useState([]);
+	const [error, setError] = useState(false);
+	
+	useEffect(() => {
+		const getAddress = async () => {
+			const result = await getAddressesForMapView(team);
+			if (result && !result.error) {
+				setTeamMembers(result);
+				setError(false);
+			} else {
+				setTeamMembers([]);
+				setError(true);
+			}
+		}
+		getAddress();
+	}, []);
+
+	console.log(teamMembers);
+
   return (
 		<>
-       <h1>
-           MAP GOES HERE!
-       </h1>
+      <h1 className='display-1 text-center my-3'>Team Member Map</h1>
+			{error && <h2 className='text-danger text-center my-3'>Error: { error }</h2>}
 			<div className='map-container'>
 				<GoogleMapReact
 					bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
 					defaultCenter={props.center}
 					defaultZoom={props.zoom}
 				>
-					<TestText
-						lat={39.9526}
-						lng={-75.1652}
-						text='Philadelphia'
-					/>
+					{teamMembers.map((teamMember, i) => {
+						return (
+							<TeamMemberAvatar
+								key={`avatar_${i}`}
+								image={teamMember.image}
+								name={teamMember.name}
+								lat={teamMember.location.lat}
+								lng={teamMember.location.lng}
+							/>
+						);
+					})}
 				</GoogleMapReact>
 			</div>
 		</>
---
+++ src/services/googleMaps.js
@@ -0,0 +1,45 @@
+const baseGoogleMapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
+
+const buildURL = address => {
+	const formattedAddress = encodeURIComponent(address);
+	return `${baseGoogleMapsUrl}address=${formattedAddress}&address=New+York,NY&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`;
+}
+
+export const getLocation = async address => {
+	try {
+		const googleMapsResponse = await fetch(buildURL(address));
+		return await googleMapsResponse.json();
+	} catch (error) {
+		return { error }
+	}
+}
+
+export const getAddressData = async address => {
+	const response = await getLocation(address);
+	if (!response || !response.results || response?.error) {
+		return { error: response?.error || 'Something went wrong' };
+	}
+	return response.results.map(item => {
+		return {
+			lat: item.geometry?.location?.lat,
+			lng: item.geometry?.location?.lng,
+			display: item.formatted_address,
+		}
+	});
+}
+
+export const addLocationDataToTeamMember = async teamMember => {
+	const addressData = await getAddressData(teamMember.location);
+	return {
+		...teamMember,
+		location: addressData[0],
+	}
+}
+
+export const getAddressesForMapView = async teamMembers => {
+	const filteredTeamMembers = teamMembers.filter(item => !!item.location);
+	const results = await Promise.all(filteredTeamMembers.map(member => {
+		return addLocationDataToTeamMember(member);
+	}));
+	return results.filter(member => !member.location?.error);
+}
