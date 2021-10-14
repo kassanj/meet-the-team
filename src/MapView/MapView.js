dfdd48fcd292e2b0cea61518fb0e1c324415cc37
TimWatervoort <twwatervoort@gmail.com>
2021-10-14 13:17:30 -0600

    adds basic stuff for mapview

---
+++ src/MapView/MapView.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import GoogleMapReact from 'google-map-react';
+import './MapView.css';
+
+const GOOGLE_MAPS_API_KEY = 'AIzaSyDpomrdhlhMJsurmyNO4VeKep9cH-UrE98';
+
+const TestText = ({ text }) => <div>{text}</div>;
+
+const MapView = (props) => {
+	const defaultCenter = {
+		lat: 40.7128,
+		lng: -74.0060,
+	}
+	const defaultZoom = 11;
+
+  return (
+		<>
+       <h1>
+           MAP GOES HERE!
+       </h1>
+			<div className='map-container'>
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY }}
+					defaultCenter={defaultCenter}
+					defaultZoom={props.zoom || defaultZoom}
+				>
+					<TestText
+						lat={39.9526}
+						lng={-75.1652}
+						text='Philadelphia'
+					/>
+				</GoogleMapReact>
+			</div>
+		</>
+  );
+}
+
+MapView.propTypes = {
+	center: PropTypes.shape({
+		lat: PropTypes.number,
+		lng: PropTypes.number,
+	}),
+	zoom: PropTypes.number,
+}
+
+MapView.defaultProps = {
+	center: {},
+	zoom: 0,
+}
+
+export default MapView;
